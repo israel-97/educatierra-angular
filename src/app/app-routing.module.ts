c2c490ccb1692db8046edb1cc776d964f2c8c510
eralima <erikalmlima@gmail.com>
2021-06-28 16:37:40 -0300

    Implementando rotas e o componente produtos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MenuComponent } from './menu/menu.component';
+import { ProdutosComponent } from './produtos/produtos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path:'home', component:MenuComponent},
+    {path:'produtos', component:ProdutosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
