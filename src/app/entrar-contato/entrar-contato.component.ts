b84014c54f16f6c42695898d3ce6e0dffcfeed92
renatons17 <83104807+renatons17@users.noreply.github.com>
2021-07-03 19:33:04 -0300

    Formulário de contato sem validação

--- src/app/entrar-contato/entrar-contato.component.ts
+++ src/app/entrar-contato/entrar-contato.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-entrar-contato',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./entrar-contato.component.css']
 })
 export class EntrarContatoComponent implements OnInit {
+  public formulario: FormGroup;
 
-  constructor() { }
 
-  ngOnInit(): void {
+   constructor(
+    private FormBuilder: FormBuilder,
+   ) { }
+
+  ngOnInit() {
+
+    this.formulario = this.FormBuilder.group({
+      nome: [null, [Validators.required,Validators.minLength(3),Validators.maxLength(100) ]],
+      email: [null, [Validators.required,Validators.email]]
+    })
   }
 
 }
+
