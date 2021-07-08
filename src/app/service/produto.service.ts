3b6990ddb5b9a4fe20d74a39899356406af45475
israel-97 <israel.dantas1@gmail.com>
2021-07-07 22:34:51 -0300

    implementando get, post de produto

--- src/app/service/produto.service.ts
+++ src/app/service/produto.service.ts
@@ -1,15 +1,16 @@
+import { Produto } from 'src/app/model/Produto';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
-import { Produto } from '../model/Produto';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProdutoService {
 
-  /*constructor(private htpp: HttpClient) { }
+  constructor(private htpp: HttpClient) { }
 
 token = {
   headers: new HttpHeaders().set('Authorization', environment.token)
@@ -22,6 +23,12 @@ refreshToken(){
 
 todosProdutos(): Observable<Produto[]>{
   return this.htpp.get<Produto[]>('http://localhost:8083/produtos', this.token)
-}*/
+}
+
+cadastrarProduto(idUsuario: number, idCategoria: number, produto: Produto): Observable<Produto> {
+  return this.htpp.post<Produto>(`http://localhost:8083/cadastro-produto/usuario/${idUsuario}/categoria/${idCategoria}`, produto, this.token)
+
+}
+
 
 }
