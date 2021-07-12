a7aeda7c5f7898bdfc8b9bce70d125d3358a5061
renatons17 <83104807+renatons17@users.noreply.github.com>
2021-07-12 00:28:20 -0300

    Página de produtos consumindo backend e implemtentação do modal de quick view

--- src/app/view-prod/view-prod.component.ts
+++ src/app/view-prod/view-prod.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
+import { Categoria } from '../model/Categoria';
+import { Produto } from '../model/Produto';
+import { User } from '../model/User';
+import { AuthService } from '../service/auth.service';
+import { CategoriaService } from '../service/categoria.service';
+import { ProdutoService } from '../service/produto.service';
 
 @Component({
   selector: 'app-view-prod',
@@ -7,9 +15,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewProdComponent implements OnInit {
 
-  constructor() { }
+  produto: Produto = new Produto()
+  categoria: Categoria = new Categoria()
+  listaProdutos: Produto[]
+  listaCategorias: Categoria[]
+  idCategoria: number
+  user: User = new User()
+  idUser = environment.id
+  idProduto: number
 
-  ngOnInit(): void {
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private produtoService: ProdutoService,
+    private categoriaService: CategoriaService,
+    private authService: AuthService
+  ) { }
+
+  ngOnInit() {
+
+    window.scroll(0,0)
+    if (environment.token == '') {
+      this.router.navigate(['/home'])
+    }
+
+    let id = this.route.snapshot.params['id']
+    this.produtoService.refreshToken()
+    this.categoriaService.refreshToken()
+    this.todasCategorias()
+    this.todosProdutos()
+  }
+
+  todasCategorias() {
+    this.categoriaService.todasCategorias().subscribe((resposta: Categoria[]) => {
+      this.listaCategorias = resposta
+      console.log(this.listaCategorias)
+    })
+  }
+
+  todosProdutos() {
+    this.produtoService.todosProdutos().subscribe((resposta: Produto[]) => {
+      this.listaProdutos = resposta
+    })
+  }
+
+  cadastrarProduto() {
+    this.produtoService.cadastrarProduto(this.idUser, this.idCategoria, this.produto).subscribe((resposta: Produto) => {
+      this.produto = resposta
+      alert('Produto cadastrado com sucesso!')
+      this.produto = new Produto()
+
+      this.todosProdutos()
+    })
+  }
+
+  favoritarProduto() {
+    this.produtoService.favoritarProduto(this.idUser, this.idProduto).subscribe((resposta: User) => {
+      this.user = resposta
+    })
   }
 
 }
