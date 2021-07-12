f7fd88f061a62638c9a81f61375fc602e8733d9e
eralima <erikalmlima@gmail.com>
2021-07-12 19:51:04 -0300

    Componente para deletar produto

---
+++ src/app/editar/produto-excluir/produto-excluir.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-produto-excluir',
+  templateUrl: './produto-excluir.component.html',
+  styleUrls: ['./produto-excluir.component.css']
+})
+export class ProdutoExcluirComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
